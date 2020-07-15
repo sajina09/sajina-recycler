acf22becf1516cad057cda49aeb52161f761c0a6
sajina09 <yourbatman9@gmail.com>
2020-07-15 23:53:50 +0545

    added on clikc listender

---
+++ app/src/main/java/app/sajina/letsstart/FacebookModel.java
@@ -0,0 +1,15 @@
+package app.sajina.letsstart;
+
+public class FacebookModel {
+
+
+    public String username;
+    public  String userDp;
+    public  String caption;
+    public  String postImage;
+    public  int likeCount;
+
+
+}
+
+
